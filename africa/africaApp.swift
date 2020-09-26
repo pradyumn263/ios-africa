f48fea2ce60276895816c90c4dc39e45fccf37db
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-09-26 11:32:36 +0530

    Initial Commit

---
+++ africa/africaApp.swift
@@ -0,0 +1,17 @@
+//
+//  africaApp.swift
+//  africa
+//
+//  Created by Pradyumn Shukla on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct africaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
